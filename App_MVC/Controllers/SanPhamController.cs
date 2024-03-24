a774f1f0e4b7b11badad047a7ae4dc50cf8c1895
Vann Trongg <vantrongvt1310@gmail.com>
2024-03-24 12:19:13 +0700

    sanpham

---
+++ App_MVC/Controllers/SanPhamController.cs
@@ -0,0 +1,56 @@
+﻿using ClassLib_App_Data.Models;
+using ClassLib_App_Data.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace App_MVC.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        WebMvcContext _context;
+        Repository<SanPham> _reps;
+        DbSet<SanPham> _dbSet;
+        public SanPhamController()
+        {
+            _context = new WebMvcContext();
+            _dbSet = _context.sanPhams;
+            _reps = new Repository<SanPham>(_dbSet,_context);
+        }
+        public IActionResult Index()
+        {
+            var getProducts = _reps.GetAll();
+            return View(getProducts);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(SanPham sanPham)
+        {
+            sanPham.ID = Guid.NewGuid();
+            _reps.CreateObj(sanPham);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(Guid id)
+        {
+            var getByID = _reps.GetByID(id);
+            return View(getByID);
+        }
+        public IActionResult UpdateProducts(SanPham sanPham)
+        {
+            _reps.UpdateObj(sanPham);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(Guid id)
+        {
+            _reps.DeleteObj(id);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Details(Guid id)
+        {
+            var getById = _reps.GetByID(id);
+            return View(getById);
+        }
+    }
+}
